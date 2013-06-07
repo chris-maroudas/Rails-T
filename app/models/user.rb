96eccc1acbf3ba762eaa56897c805e678afd79d1
mallibu <mallibu97@gmail.com>
2013-06-07 05:18:43 +0300

    Make a basic user model with validations and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,20 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+
+  before_save :prepare_email
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+	validates :name, presence: true, length: { maximum:  50 }
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+
+
+  private
+
+	def prepare_email
+		self.email = self.email.strip.downcase if self.email
+	end
+
 end
