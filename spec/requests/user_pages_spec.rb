37eff7d4d5d85812dc1a506cebcb28e7aa999390
mallibu <mallibu97@gmail.com>
2013-06-09 02:16:58 +0300

    add view, tests with factory girl and lowered bcrypts cost in test enviroment

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,4 +13,15 @@ describe "UserPages" do
 
 	end
 
+
+
+	describe "Profile Page" do
+		let(:user) { FactoryGirl.create(:user) }
+		before { visit user_path(user) }
+
+		it { should have_selector('h1', text: user.name)}
+		it { should have_selector('title', text: user.name)}
+
+	end
+
 end
