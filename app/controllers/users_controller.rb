eb17db96a59b728ba581aac9a315625114831b70
mallibu <mallibu97@gmail.com>
2013-06-09 03:56:37 +0300

    sign up success, errors and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ApplicationController
 
   def new
+		@user = User.new
+  end
+
+  def create
+		@user = User.new(params[:user])
+		if @user.save
+			redirect_to @user
+			flash[:success] = 'Thank you for signing up'
+		else
+			render 'new'
+		end
   end
 
 	def show
